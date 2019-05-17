ff6b07b1af65115c1c869ac305e0dc1c5a299797
Kellen Baker <mkellenbaker@gmail.com>
2019-05-17 16:43:47 -0500

    added some functionality to submit button

--- client.js
+++ client.js
@@ -2,4 +2,24 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('henlo');
+    $('#submitEmployeeInfoButton').on('click', handleAddClick);
+}
+
+function handleAddClick(){
+    console.log('in handleAddClick');
+    let firstNameIn = $('#firstNameInput').val();
+    let lastNameIn = $('#lastNameInput').val();
+    let idIn = $('#idNumberInput').val();
+    let titleIn = $('#titleInput').val();
+    let annualSalaryIn = $('#annualSalaryInput').val();
+    let employeeRow = `<tr>
+                <td>${firstNameIn}</td>
+                <td>${lastNameIn}</td>
+                <td>${idIn}</td>
+                <td>${titleIn}</td>
+                <td>${annualSalaryIn}</td>
+                <td><button class="deleteEmployee">Delete</button></td>
+            </tr>`
+    $('tbody').append(employeeRow);
+
 }
