a65db4651a1ae7ff5cff2fa0e8b54151dcee0a81
Kellen Baker <mkellenbaker@gmail.com>
2019-05-19 10:38:51 -0500

    DOM displays correct monthly cost

--- client.js
+++ client.js
@@ -26,11 +26,9 @@ function handleAddClick() {
     //run addMonthlyCost function?? 
     let monthlyCost = addMonthlyCosts();
 
-    monthlyCost = Number(monthlyCost) + Number(annualSalaryIn);
-
     console.log(monthlyCost);
 
-    $('#monthlyCost').text(monthlyCost);
+    $('#monthlyCost').text(Math.round(monthlyCost/12));
 
     //clear input values
     $('input').val('');
@@ -60,29 +58,10 @@ function addMonthlyCosts() {
     console.log(salaryArrayNumbers);
     //loop thru array and add values
     for (i = 0; i < salaryArrayNumbers.length; i++) {
-        yearlySum = Number(yearlySum) + Number(salaryArrayNumbers[i]);
+        yearlySum = yearlySum + salaryArrayNumbers[i];
     }
     //return sum of all values 
     return yearlySum;
-
-
-
-
-
-    // //the following code adapted from https://stackoverflow.com/questions/9293492/sum-all-values-for-table-column-based-on-class
-    // //further research needed
-    // let sum=0;
-    // // iterate through each td based on class and add the values
-    // $(".salaryColumn").each(function () {
-    //     let value = $(this).text();
-    //     // add only if the value is number
-    //     if (!isNaN(value) && value.length != 0) {
-    //         sum += parseFloat(value);
-    //     }
-    // });
-    // ;
-    // return sum;
-    // //end adapted copy code 
 }
 //add total of annual salary column
 //divide cost by 12
